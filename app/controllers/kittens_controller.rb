ac8069ca3354cf5a35b118f79ba2ea6fdeba7d6f
ClaireCompare <claire@markcompare.com>
2025-06-02 14:05:05 +0200

    Create a new kitten resource with basic CRUD functionality -Add a new resource for kittens with the following features: - Create a new kitten with attributes like name, age, and color. - Read a list of all kittens and view individual kitten details. - Update kitten details. - Delete a kitten from the database.

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,48 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Kitten was successfully created."
+      redirect_to @kitten
+    else
+      flash.now[:error] = "You're an idiot."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: "Kitten was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    redirect_to root_path, notice: "Kitten was successfully deleted."
+  end
+
+  private
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
